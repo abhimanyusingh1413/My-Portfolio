a6c0fd49370618b7f2021eb1c23647185145f550
abhimanyusingh1413 <130711724+abhimanyusingh1413@users.noreply.github.com>
2024-12-12 15:15:59 +0530

    updating portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import NavBar from "./components/NavBar.jsx"
+import Home from "./components/Home.jsx"
+import SocialLinks from "./components/SocialLinks.jsx"
+import About from "./components/About.jsx"
+import Portfolio from "./components/Portfolio.jsx"
+import Experience from "./components/Experience.jsx"
+import Contact from "./components/Contact.jsx"
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar/>
+      <Home/>
+      <About/>
+      <Portfolio/>
+      <Experience/>
+      <Contact/>
+
+      <SocialLinks/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about"
+    className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+            <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+        </div>
+            <p className="text-xl mt-20">I am a devoted engineering student with a solid background in web development and programming, as well as a love of technology. I have developed my abilities in database administration and creating responsive applications, and I have a CGPA of 7.64 with a computer science concentration. Using React.js and Tailwind CSS, I recently created a portfolio website that demonstrates my dedication to lifelong learning and teamwork.
+            </p>
+
+            <br />
+
+            <p className="text-xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repudiandae inventore porro! Iusto maxime ipsam quas beatae earum culpa non alias quos voluptate adipisci. Quaerat, obcaecati dignissimos qui voluptate minima voluptatum odio harum quia culpa mollitia quos exercitationem rerum natus voluptatem. Incidunt, in accusamus? At, expedita! Nobis beatae enim labore.
+            </p>
+      </div>
+    </div>
+  )
+}
+
+export default About
